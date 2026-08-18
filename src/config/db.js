91c8a70c442551ab893cace17714f70d08283d8b
Luis Rojas <luisjoserojas08@gmail.com>
2026-08-18 11:55:06 -0400

    Forzar ipv4first en dns para evitar ENETUNREACH en Render

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,6 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-// Imprimimos para verificar en los logs de Render si la URL está llegando
 console.log("DATABASE_URL configurada:", process.env.DATABASE_URL ? "SÍ ESTÁ PRESENTE" : "ESTÁ VACÍA O FALTANTE");
 
 const pool = new Pool({
@@ -9,7 +8,9 @@ const pool = new Pool({
   ssl: {
     rejectUnauthorized: false
   },
-  family: 4 // Fuerza IPv4
+  family: 4, // Fuerza IPv4 a nivel de pool
+  // Opciones adicionales para evitar que intente usar sockets IPv6
+  keepAlive: true,
 });
 
 module.exports = pool;

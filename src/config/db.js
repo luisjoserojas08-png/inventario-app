6a08128a9713ca6dcb4c8c2e54837343570c6b8a
Luis Rojas <luisjoserojas08@gmail.com>
2026-08-18 10:56:04 -0400

    Forzar IPv4 en conexion de postgres

--- src/config/db.js
+++ src/config/db.js
@@ -2,15 +2,18 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
-
-pool.on('connect', () => {
-  console.log('Base de datos conectada con éxito');
+  port: process.env.DB_PORT || 5432,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  // Forzar el uso de IPv4 para evitar el error de red en Render
+  connectionTimeoutMillis: 5000,
+  keepAlive: true,
+  family: 4 
 });
 
 module.exports = pool;

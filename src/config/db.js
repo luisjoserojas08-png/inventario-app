7e75f4b51bee359d452718f23aa1de2bf4651102
Luis Rojas <luisjoserojas08@gmail.com>
2026-08-18 12:02:43 -0400

    Ajustar conexion a puerto directo 5432

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,12 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-console.log("DATABASE_URL configurada:", process.env.DATABASE_URL ? "SÍ ESTÁ PRESENTE" : "ESTÁ VACÍA O FALTANTE");
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   },
-  family: 4, // Fuerza IPv4 a nivel de pool
-  // Opciones adicionales para evitar que intente usar sockets IPv6
-  keepAlive: true,
+  family: 4
 });
 
 module.exports = pool;
